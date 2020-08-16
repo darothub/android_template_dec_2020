fa2af589bcb4fa7d214bdb0a59b9371169eaffb9
Mac <darotudeen@gmail.com>
2020-08-16 09:41:41 +0100

    added animation

--- app/src/main/java/com/peacedude/lassod_tailor_app/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/peacedude/lassod_tailor_app/ui/SplashScreenActivity.kt
@@ -1,20 +1,32 @@
 package com.peacedude.lassod_tailor_app.ui
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.annotation.RequiresApi
 import com.peacedude.lassod_tailor_app.R
+import com.peacedude.lassod_tailor_app.helpers.buildVersion
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
     val handler by lazy {
         Handler(Looper.getMainLooper())
     }
+    val topAnimation by lazy{
+        AnimationUtils.loadAnimation(this, R.anim.top_animation)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        app_logo.animation = topAnimation
 
 
     }
@@ -24,27 +36,26 @@ class SplashScreenActivity : AppCompatActivity() {
         handler.postDelayed ({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2000)
+        }, 5000)
     }
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
 //        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE
         if (hasFocus) hideSystemUI()
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
     private fun hideSystemUI() {
-        window.setDecorFitsSystemWindows(false)
-//        // Enables regular immersive mode.
-//        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-//        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-//                // Set the content to appear under the system bars so that the
-//                // content doesn't resize when the system bars hide and show.
-//                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                // Hide the nav bar and status bar
-//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        buildVersion({
+            window.setDecorFitsSystemWindows(false)
+        },{
+            // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        })
+
+
     }
 }
