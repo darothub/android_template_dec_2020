c743576e2f3081e5d2f30928d2dfdc6d08d4d901
darot <darotudeen@gmail.com>
2020-12-01 10:12:00 +0100

    added drawable click listener for single chat edit text

--- app/src/main/java/com/peacedude/lassod_tailor_app/ui/SingleChatFragment.kt
+++ app/src/main/java/com/peacedude/lassod_tailor_app/ui/SingleChatFragment.kt
@@ -1,21 +1,21 @@
 package com.peacedude.lassod_tailor_app.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import com.peacedude.lassod_tailor_app.R
 import com.peacedude.lassod_tailor_app.helpers.getName
-import com.peacedude.lassod_tailor_app.helpers.goto
 import com.peacedude.lassod_tailor_app.helpers.i
 import com.vanniktech.emoji.EmojiPopup
 import dagger.android.support.DaggerFragment
-import hani.momanii.supernova_emoji_library.Actions.EmojIconActions
-import hani.momanii.supernova_emoji_library.Actions.EmojIconActions.KeyboardListener
 import kotlinx.android.synthetic.main.fragment_single_chat.*
 
 
@@ -29,6 +29,7 @@ class SingleChatFragment : DaggerFragment() {
     val title: String by lazy {
         getName()
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,14 +43,15 @@ class SingleChatFragment : DaggerFragment() {
         return inflater.inflate(R.layout.fragment_single_chat, container, false)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val  emojiPopup = EmojiPopup.Builder.fromRootView(single_chat_root_view)
+        val emojiPopup = EmojiPopup.Builder.fromRootView(single_chat_root_view)
             .setOnEmojiBackspaceClickListener { Log.d(title, "Clicked on Backspace") }
-            .setOnEmojiClickListener { emoji, imageView -> Log.d(title, "Clicked on emoji")  }
-            .setOnSoftKeyboardOpenListener {  Log.d(title, "Opened soft keyboard")}
-            .setOnSoftKeyboardCloseListener {  Log.d(title, "Closed soft keyboard")}
+            .setOnEmojiClickListener { emoji, imageView -> Log.d(title, "Clicked on emoji") }
+            .setOnSoftKeyboardOpenListener { Log.d(title, "Opened soft keyboard") }
+            .setOnSoftKeyboardCloseListener { Log.d(title, "Closed soft keyboard") }
             .setOnEmojiPopupShownListener { single_chat_emoji_iv.setImageResource(R.drawable.ic_baseline_keyboard_24) }
             .setOnEmojiPopupDismissListener { single_chat_emoji_iv.setImageResource(R.drawable.ic_baseline_insert_emoticon_24) }
             .build(single_chat_emoji_et)
@@ -62,25 +64,27 @@ class SingleChatFragment : DaggerFragment() {
             startActivity(Intent(requireContext(), DashboardActivity::class.java))
         }
 
-//        val emojIcon = EmojIconActions(
-//            requireContext(),
-//            single_chat_root_view,
-//            single_chat_emoji_et,
-//            single_chat_emoji_iv
-//        )
-//        emojIcon.setKeyboardListener(object : KeyboardListener {
-//            override fun onKeyboardOpen() {
-//                Log.e("Keyboard", "open")
-//            }
-//
-//            override fun onKeyboardClose() {
-//                Log.e("Keyboard", "close")
-//            }
-//        })
         single_chat_send_fab.setOnClickListener {
             val newText: String = single_chat_emoji_et.text.toString()
 
         }
+
+        single_chat_emoji_et.setOnTouchListener { v, event ->
+            val DRAWABLE_LEFT = 0
+            val DRAWABLE_TOP = 1
+            val DRAWABLE_RIGHT = 2
+            val DRAWABLE_BOTTOM = 3
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= single_chat_emoji_et.right - single_chat_emoji_et.compoundDrawables[DRAWABLE_RIGHT].bounds.width()
+                ) {
+                    // your action here
+                    i(title, "Drawable touched")
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
+
 
         requireActivity().onBackPressedDispatcher.addCallback {
             startActivity(Intent(requireContext(), DashboardActivity::class.java))
@@ -90,36 +94,3 @@ class SingleChatFragment : DaggerFragment() {
 }
 
 
-//<hani.momanii.supernova_emoji_library.Helper.EmojiconEditText
-//android:id="@+id/emojicon_edit_text"
-//android:layout_width="match_parent"
-//android:layout_height="wrap_content"
-//android:layout_alignParentBottom="true"
-//android:layout_toLeftOf="@id/submit_btn"
-//android:layout_toRightOf="@id/emoji_btn"
-//android:imeOptions="actionSend"
-//android:inputType="text"
-//emojicon:emojiconSize="28sp"/>
-//
-//
-//<hani.momanii.supernova_emoji_library.Helper.EmojiconEditText
-//android:id="@+id/emojicon_edit_text2"
-//android:layout_width="match_parent"
-//android:layout_height="wrap_content"
-//android:layout_above="@id/emojicon_edit_text"
-//android:imeOptions="actionSend"
-//android:inputType="text"
-//emojicon:emojiconSize="28sp"/>
-
-//<hani.momanii.supernova_emoji_library.Helper.EmojiconTextView
-//android:id="@+id/textView"
-//android:layout_width="wrap_content"
-//android:layout_height="wrap_content"
-//android:layout_centerHorizontal="true"
-//android:layout_centerVertical="true"
-//android:layout_marginTop="26dp"
-//android:lineSpacingExtra="17sp"
-//android:text="Hello Emojis !"
-//android:textAppearance="@style/TextAppearance.AppCompat.Large"
-//android:textColor="#000000"
-//emojicon:emojiconAlignment="bottom"/>
